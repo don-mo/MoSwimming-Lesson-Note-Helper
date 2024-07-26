f879d03bf62799de572802e33e18c9135f640fc4
Don <donmo99@gmail.com>
2024-07-26 14:56:37 -0700

    First Commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        // main menu loop
+        // Add anilities
+        // Remove abilities
+        // View abilities
+        // Load abilities
+        // Save abilities
+        // Quit
+        System.out.println("Welcome to MoSwimming Instructor Progress Tracker Version 0.0.0!");
+        String userInput = "";
+
+        while (!userInput.equals("quit")) {
+            System.out.print(
+                    "What option would you like? \n(c)reate a new progress \n(a)dd a new skill \n(d)isplay skills \n(de)lete a skill (ad)d a new category (q)uit");
+
+        }
+
+    }
+
+}
