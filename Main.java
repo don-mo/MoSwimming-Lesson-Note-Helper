7738f002e41ba22c5383b8ca36218cd7a9bff1bc
Don <donmo99@gmail.com>
2024-07-28 14:52:48 -0700

    created better main menu and while loop doesn't infinite loop

--- Main.java
+++ Main.java
@@ -12,12 +12,38 @@ public class Main {
         // Load abilities
         // Save abilities
         // Quit
+        Scanner console = new Scanner(System.in);
         System.out.println("Welcome to MoSwimming Instructor Progress Tracker Version 0.0.0!");
         String userInput = "";
+        int commands = 0;
+        System.out.println(
+                "What option would you like? (Type the letters in parentheses, lowercase or uppercase is fine) ");
+        System.out.println("(C)reate a new progress");
+        System.out.println("(A)dd a new skill");
+        System.out.println("(D)isplay skills");
+        System.out.println("(De)lete a skill");
+        System.out.println("(Ad)d a new category");
+        System.out.println("(Q)uit");
+        System.out.println();
+        System.out.print("Answer here: ");
+        userInput = console.next();
 
-        while (!userInput.equals("quit")) {
-            System.out.print(
-                    "What option would you like? \n(c)reate a new progress \n(a)dd a new skill \n(d)isplay skills \n(de)lete a skill (ad)d a new category (q)uit");
+        while (!userInput.equalsIgnoreCase("quit")) {
+            // if c
+            // if a
+            // if d
+            // if de
+            // if ad
+            // else
+            // } else if (!userInput.equalsIgnoreCase("q")) {
+            // System.out.println("Unknown input: " + userInput);
+            // }
+            // if (!userInput.equalsIgnoreCase("q")) {
+            // printTodos(toDoList);
+            // }
+            // }
+
+            userInput = "quit";
 
         }
 
